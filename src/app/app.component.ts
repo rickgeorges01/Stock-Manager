b78954024528f01ef5dc1db97cf31d80fcc0106b
rickgeorges <94584894+rickgeorges01@users.noreply.github.com>
2023-12-20 00:28:58 +0100

    Interface inscription et connexion réalisé !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {PageLoginComponent} from "./page-login/page-login.component";
+import {PageInscriptionComponent} from "./page-inscription/page-inscription.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PageLoginComponent, PageInscriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
